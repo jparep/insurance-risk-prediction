5d2788b81dd31a3df2c1b8a5b08840191e824c00
jparep <parepson@gmail.com>
2024-12-05 19:34:05 -0700

    Loggin added

--- model_development/scripts/insurance_risk_prediction.py
+++ model_development/scripts/insurance_risk_prediction.py
@@ -1,4 +1,21 @@
 # Import libraries
-import pandas as np
+import os
+import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+# Gloabal Variables
+# Construct a relative path to the data dir
+DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
+
+# Get File path
+FILE_PATH = os.path.join(DIR, 'insurance.csv')
+
+# Load Data
+df = pd.read_csv(FILE_PATH)
+
+# Read First 5 rows
+print(df.head())
