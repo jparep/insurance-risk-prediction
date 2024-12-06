154a1232fae3528e0574cf493ff306dce54870b7
jparep <parepson@gmail.com>
2024-12-05 20:16:52 -0700

    Preprocessed data

--- model_development/scripts/insurance_risk_prediction.py
+++ model_development/scripts/insurance_risk_prediction.py
@@ -27,12 +27,65 @@ def load_data(file_path):
         logging.info(f'Data Successfully loadded from file {file_path}')
         return df
     except Exception as e:
-        logging.error(f"Issue loadding data from file {file_path}")
+        logging.error(f"Issue loadding data from file {file_path}: {str(e)}")
+
+def preprocess_data(df):
+    """Preprocess data"""
+    # Remove $ and convert to Int from object
+    df['charges'] = df['charges'].replace({'\$': ''}, regex=True).astype(float)
+    # Covert negative age values to positives
+    df['age'] = abs(df['age'])
+    
+    # Convert children to Int from object type
+    df['children'] = df['children'].astype('int64')
+    # Convert neagtive number of children to positive
+    df['children'] = abs(df['children'])
+    
+    # Map sex to male and female
+    sex_mapping = {
+        'female': 'Female',
+        'woman': 'Female',
+        'Woman': 'Female',
+        'F': 'Female',
+        'f': 'Female',
+        'male': 'Male',
+        'man': 'Male',
+        'Man': 'Male',
+        'M': 'Male',
+        'm':'Male',
+        }
+
+    # Appply mapping on sex
+    df['sex'] = df['sex'].map(sex_mapping)
+    
+    # Map Region
+    region_map = {
+        'southwest': 'Southwest',
+        'southeast': 'Southeast',
+        'northwest': 'Northwest',
+        'northeast': 'Northeast'
+    }
+    # Apply mapping on region
+    df['region'] = df['region'].map(region_map)
+    
+    # Remove rows that are entirely empty accross the columns
+    df = df.dropna(how='all').reset_index(drop=True)
+    
+    # If 5 out of 7 coulmns as empty values in the same row index, drop them
+    df = df[df.isnull().sum() > 5].reset_index(drop=True)
+    
+    # Remove NAs from target: It will also remove values from features in the same row index
+    df = df.dropna(subset=['target']).set_index(drop=True)
+    return df
 
 def main():
     # Load data
-    df = load_data(FILE_PATH)
-    print(df.head())
+    try:
+        df = load_data(FILE_PATH)
+        df_cleaned = preprocess_data(df)
+        print(df_cleaned.head())
+    except Exception as e:
+        logging.critical(f"Error in main: {str(e)}")
 
 if __name__=='__main__':
     main()
