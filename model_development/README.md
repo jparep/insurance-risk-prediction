15d425c061aeb3d1baa6857f9e613c3359c6134a
jparep <parepson@gmail.com>
2024-12-06 03:46:31 -0700

    logger file created

---
+++ model_development/README.md
@@ -0,0 +1,13 @@
+# Insurance Model Development
+
+## Overview
+This project builds a Ridge regression model to predict insurance charges using demographic and regional data.
+
+## Directory Structure
+- `data/`: Contains the dataset.
+- `models/`: Stores the trained model.
+- `scripts/`: Contains the main Python script.
+- `logs/`: Application log files.
+
+## Setup
+1. Install dependencies:
--- model_development/utils/logger.py
+++ model_development/utils/logger.py
@@ -1,3 +1,17 @@
 import logging
 import os
-from config import LOGFILE
+from config import LOG_FILE
+
+def setup_logger():
+    """
+    Configures loggin for the project.
+    """
+    
+    os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        handlers=[logging.StreamHandler, logging.FileHandler(LOG_FILE)]
+    )
+    
+    logging.info('Logger initalized.')
