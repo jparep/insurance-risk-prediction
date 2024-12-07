93a219b9ed1ff4eafc3ff546cd208c0eaf20932e
jparep <parepson@gmail.com>
2024-12-06 18:18:30 -0700

    .env created

--- web_api/app/__init__.py
+++ web_api/app/__init__.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env
 
 def create_app():
     app = Flask(__name__)
-    app.config(['SECRETE_KEY'] = 'your-secrete-key')
+    
+    # Use the environemnt variabel or a fallback for local dev
+    app.config['SECRETE_KEY'] = os.getenv('SECRETE_KEY', 'fallback-secret-key')
     
     from .routes import main
     app.register_blueprint(main)
