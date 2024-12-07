3d7d978cf1ccd48c1e4eb66879999e5b2dbcfa5e
jparep <parepson@gmail.com>
2024-12-06 20:10:31 -0700

    Flask route file created

--- web_api/app/routes.py
+++ web_api/app/routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request
 import joblib
+import pandas as pd
 
 main = Blueprint('main', __name__)
 
 # Load the pre-trained model
-model = joblib.load('../model_development/models/model.joblib')
+model_pipeline = joblib.load('../model_development/models/model_pipeline.joblib')
 
 @main.route('/')
 def home():
@@ -12,8 +13,29 @@ def home():
 
 @main.route('/predict', methods=['POST'])
 def predict():
-    # Get usr input from form
-    age = int(request.form['age'])
-    bmi = float(request.form['bmi'])
-    children = int(request.form['children'])
-    smoker = 1 if request.form['smoker'] == 'yes' else 0
+    try:
+        # Get user input from form
+        age = int(request.form['age'])
+        bmi = float(request.form['bmi'])
+        children = int(request.form['children'])
+        smoker = 1 if request.form['smkoer'] == 'yes' else 0
+        sex = request.form['sex']
+        region = request.form['region']
+        
+        # prepare input as a DataFrame
+        input_data = {
+            'age': [age],
+            'bmi': [bmi],
+            'children': [children],
+            'smoker': [smoker],
+            'sex': [sex],
+            'region': [region],
+        }
+        
+        # prediction using the loaded pipeline
+        prediction = model_pipeline.predict(pd.DataFrame(input_data))[0]
+        
+        return render_template('result.html', prediciton=round(prediction, 2))
+    
+    except Exception as e:
+        return f"An error occured: {str(e)}"
